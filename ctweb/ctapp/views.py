e5a658ad201c608ba59dc38fa32b08f9aeb5a203
manas-16 <62739157+manas-16@users.noreply.github.com>
2021-03-11 12:37:53 +0530

    4th change

--- ctweb/ctapp/views.py
+++ ctweb/ctapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import teacher,student,subject,teacher_assign,student_submission,assignment,Class
 
 # Create your views here.
 def index(request):
     template = loader.get_template('index.html')
-    context = {'name':"batman"}
+    teachers = teacher_assign.objects.all()
+    st = student.objects.all()
+    subjects = subject.objects.all()
+    context = {'name':"batman","teachers":teachers,"student":st,"sub":subjects}
     return HttpResponse(template.render(context, request))
