c8e53f9b7fa1cc04c9bc94286ea5017a2cb61477
anushreekumar20 <70310471+anushreekumar20@users.noreply.github.com>
2021-03-18 00:31:38 +0530

    Update forms.py

--- ctweb/ctapp/forms.py
+++ ctweb/ctapp/forms.py
@@ -5,8 +5,20 @@ from .models import teacher
 class StudentForm(ModelForm):
     class Meta:
         model = student
-        fields = '__all__'
-
+        fields = {'user', 'enrollment_number' , 'name' , 'college_name' , 'sem' , 'sec' , 'branch' , 'mobile_no' , 'email'}
+        '''
+        widgets = {
+        'user' : ModelForm.TextInput(attrs={'class': 'form-control'}),
+        'enrollment_number' : ModelForm.TextInput(attrs={'class': 'form-control'}),
+        'name' : ModelForm.TextInput(attrs={'class': 'form-control'}),
+        'college_name' : ModelForm.TextInput(attrs={'class': 'form-control'}),
+        'sem' : ModelForm.TextInput(attrs={'class': 'form-control'}),
+        'sec' : ModelForm.TextInput(attrs={'class': 'form-control'}),
+        'branch' : ModelForm.TextInput(attrs={'class': 'form-control'}),
+        'mobile_no' : ModelForm.TextInput(attrs={'class': 'form-control'}),
+        'email' : ModelForm.TextInput(attrs={'class': 'form-control'}),
+        }
+'''
 class TeacherForm(ModelForm):
     class Meta:
         model = teacher
